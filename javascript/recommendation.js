ea33ae998f6c834325ab73a568ad2ec40d6ff84d
sejslsharmas <sejalsharmaany@gmail.com>
2024-07-29 18:47:07 +0530

    path set

--- javascript/recommendation.js
+++ javascript/recommendation.js
@@ -77,3 +77,30 @@ function displayResult() {
 }
 
 window.onload = loadQuestion;
+
+
+
+
+
+
+
+document.getElementById('dropdown-items').addEventListener('click', function() {
+    document.getElementById('navMenu').classList.toggle('active');
+});
+
+
+
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeIcon.addEventListener('click', () => {
+    if (body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        themeIcon.src = '../images/arts-page-image/moon.png';
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        themeIcon.src = '../images/arts-page-image/sun.png';
+    }
+});
